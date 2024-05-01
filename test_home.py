9c12b76f101595951c62778f706e6a9154157bfe
Talari Pavan Kumar <talaripavankumar0103@gmail.com>
2024-05-01 21:20:56 +0530

    Creating a basic streamlit PDF Reader.

--- test_home.py
+++ test_home.py
@@ -1,13 +1,5 @@
-from core.agent_builder.base import RAGAgentBuilder
-from llama_index.vector_stores.milvus import MilvusVectorStore
-from llama_index.core import StorageContext , VectorStoreIndex
+from llama_index.core import SimpleDirectoryReader ,Document
 
-load = RAGAgentBuilder()
-agent = load.load_data(directory="data")
-vector_store = MilvusVectorStore(dim=1536, collection_name="llamacollection" , overwrite=False)
-print("Milvus Created")
-storage_context = StorageContext.from_defaults(vector_store=vector_store)
-index = VectorStoreIndex.from_documents(
-    documents=agent, storage_context=storage_context
-)
-print("Vector Index")
+documents = SimpleDirectoryReader("data").load_data()
+print("Document ID:", documents[0].doc_id)
+
