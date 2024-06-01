1c14350bfcfb15f72d41779801122e337fc66917
Talari Pavan Kumar <95948550+talaripavan@users.noreply.github.com>
2024-06-02 03:08:09 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -2,18 +2,26 @@ from llama_index.core.agent import FunctionCallingAgentWorker
 from llama_index.core.agent import AgentRunner
 from llama_index.core import SimpleDirectoryReader
 from llama_index.vector_stores.milvus import MilvusVectorStore
-from llama_index.core import VectorStoreIndex,StorageContext
+from llama_index.core import VectorStoreIndex,StorageContext,get_response_synthesizer
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.core.tools import FunctionTool, QueryEngineTool
 from llama_index.llms.openai import OpenAI
+from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.core.query_engine import RetrieverQueryEngine
+from llama_index.core.postprocessor import SimilarityPostprocessor
+from llama_index.core.postprocessor import SentenceTransformerRerank
+
+
 import os
-import streamlit as st
+
 from dotenv import load_dotenv
 load_dotenv()
 
 def save_uploaded_files(uploaded_files):
+    
     file_paths = []
     upload_directory = "uploaded_files"
+    
     if not os.path.exists(upload_directory):
         os.makedirs(upload_directory)
         
@@ -24,14 +32,16 @@ def save_uploaded_files(uploaded_files):
         with open(file_path, "wb") as f:
             f.write(uploaded_file.getvalue())
         #st.write(f"File saved: {file_path}")
+        
     return file_paths
 
-def load_data(files):
+
+def save_file_and_store_index(files):
     reader = SimpleDirectoryReader(input_files=files)
     docs = reader.load_data()
     spilter = SentenceSplitter(chunk_size=1024)
     chunk = spilter.get_nodes_from_documents(documents=docs)
-    vector_store = MilvusVectorStore(collection_name="llamacollection",dim=1536,overwrite=True,uri="< URL >")
+    vector_store = MilvusVectorStore(collection_name="llamacollection",dim=1536,overwrite=True,uri="http://192.168.29.44:19530")
     storage_context = StorageContext.from_defaults(vector_store=vector_store)
     vector_index = VectorStoreIndex(nodes=chunk,storage_context=storage_context)
     return vector_index
@@ -40,8 +50,8 @@ def create_agent(agent_name):
     name = agent_name
     return f"Agent is created successfully {name}."
 
-def load_data_agent():
-    data_tool = FunctionTool.from_defaults(fn=load_data)
+def load_agent():
+    data_tool = FunctionTool.from_defaults(fn=save_file_and_store_index)
     create_agent_tool = FunctionTool.from_defaults(fn=create_agent)
     llm = OpenAI(model="gpt-3.5-turbo")
     agent_worker = FunctionCallingAgentWorker.from_tools(
@@ -65,14 +75,34 @@ def load_data_agent():
     load_agent = AgentRunner(agent_worker)
     return load_agent
 
-def retrieve_data_agent(files):
-    vector_index = load_data(files=files)
-    vector_query_engine = vector_index.as_query_engine()
+
+def retrieve_data_agent(collection_name):
+    vector_store = MilvusVectorStore(collection_name=collection_name, dim=1536, overwrite=False, uri="http://192.168.29.44:19530")
+    index = VectorStoreIndex.from_vector_store(vector_store)
+    
+    # Method 1
+    vector_query_engine = index.as_query_engine(similarity_top_k = 2)
+    
+    # Method 2
+    retriever = VectorIndexRetriever(index=index,similarity_top_k=5)
+    response_synthesizer = get_response_synthesizer()
+    rerank = SentenceTransformerRerank(
+        model="cross-encoder/ms-marco-MiniLM-L-2-v2", top_n=3)
+    retriever_engine = RetrieverQueryEngine(
+        retriever=retriever,
+        response_synthesizer=response_synthesizer,
+        #node_postprocessors=[SimilarityPostprocessor(similarity_cutoff=0.7)] 
+         # add rerank here .
+        node_postprocessors=[rerank]
+    )
+    
     vector_tool = QueryEngineTool.from_defaults(
-        query_engine=vector_query_engine,
-        description= f"Useful for retrieving specific context from {files}."
+        query_engine=retriever_engine,
+        description= " Tool for Semantic Search using the Vector Store "
         )
+    
     llm = OpenAI(model="gpt-3.5-turbo")
+    
     agent_worker = FunctionCallingAgentWorker.from_tools(
         tools=[vector_tool],
         llm=llm,
@@ -94,4 +124,12 @@ def retrieve_data_agent(files):
         )
     retrieve_agent = AgentRunner(agent_worker)
     return retrieve_agent
+
+
+collection_name = os.getenv('COLLECTION_NAME')
+
+agent = retrieve_data_agent(collection_name)
+response = agent.chat(" Summarize the whole PDF 'ts-eamcet-2022-last-rank (1).pdf' ")
+print(response)
+
 
